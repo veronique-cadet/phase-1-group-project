aa3183ffa4792db719251f9590dae6d87133e9d1
Adam Fisher <amgfisher90@gmail.com>
2022-12-05 18:00:34 -0500

    Initial Javascript fetch

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("this is connected!");
+
+fetch('http://localhost:3000/codes')
+.then(resp => resp.json())
+.then(qrCodes => qrCodes.forEach(generateWallet))
+
+function generateWallet(qrCode) {
+    console.log("data is connected!")
+    console.log(qrCode)
+    const walletDiv = document.getElementById('item-list');
+    const walletItem = document.createElement('p')
+    walletItem.className = 'items'
+    walletDiv.append(walletItem)
+    walletItem.innerText = qrCode.name
+    walletItem.addEventListener('click', () => {
+        console.log("clicked!")
+        
+    })
+}
