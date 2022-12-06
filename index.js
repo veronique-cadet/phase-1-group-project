927095d019848c44968a26fad56636bb36472d92
Adam Fisher <amgfisher90@gmail.com>
2022-12-06 15:31:12 -0500

    Tuesday MVP Level

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 fetch("http://localhost:3000/codes")
-  .then((resp) => resp.json())
-  .then((qrCodes) => qrCodes.forEach(generateWallet));
+.then((resp) => resp.json())
+.then((qrCodes) => qrCodes.forEach(generateWallet));
 
 function generateWallet(qrCode) {
   const walletDiv = document.getElementById("item-list");
@@ -14,6 +14,8 @@ function generateWallet(qrCode) {
 }
 const itemName = document.getElementById("item-name");
 const itemImage = document.getElementById("image-detail");
+const description = document.getElementById("item-description");
+const url = document.getElementById("item-url");
 
 renderMainDetail = (item) => {
   itemName.innerText = item.name;
@@ -21,35 +23,49 @@ renderMainDetail = (item) => {
 
   itemName.addEventListener("mouseover", () => {
     console.log("hiiiii");
-    const description = document.createElement("p");
-    const url = document.createElement("p");
     description.className = "mouse";
     url.className = "mouse";
     description.innerText = item.description;
-    itemName.append(description, url);
-
-    description.innerText = item.description;
     url.innerText = item.url;
     itemName.addEventListener("mouseout", () => {
-      description.remove();
-      url.remove();
+      description.textContent = "";
+      url.textContent = "";
     });
   });
 };
 
-const urlInput = encodeURI(document.getElementById("url").value);
+const urlInput = document.getElementById("url");
 const nameInput = document.getElementById("name");
+const descriptionInput = document.getElementById("description")
 const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("submit worked");
+  // const urlInput= e.target.url.value
   async function request() {
     let req = await fetch(
-      `https://api.qrserver.com/v1/create-qr-code/?data=${urlInput}&size=100x100`
+      `https://api.qrserver.com/v1/create-qr-code/?data=${urlInput.value}&size=250x250`
     );
     //   let resp = await req.json()
     console.log(req);
+    console.log(req.url)
+    itemImage.src = req.url
+    const newQr = {
+      "name": nameInput.value,
+      "qrcode": req.url,
+      "image": req.url,
+      "description": descriptionInput.value,
+      "url": urlInput.value
+    }
+    fetch('http://localhost:3000/codes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newQr)
+    })
+    generateWallet(newQr)
   }
   request();
 });
